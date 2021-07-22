0e38b07b0b638064e1ecef7bb046cb469141028d
ueslen santos <ueslencriacao@gmail.com>
2021-07-22 20:28:56 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '608615f1add5df858f4be895d33692d9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '890ddb10-3ef5-45bd-8129-01b6498ff391'
+};
